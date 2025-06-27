3fc730db6126ce5f0f594be646be00589b67898e
Ratnadeep Roy <61830831+Ratnadeeproy25@users.noreply.github.com>
2025-06-28 00:52:17 +0530

    receipt update

--- Backend/controllers/reimbursementController.js
+++ Backend/controllers/reimbursementController.js
@@ -542,6 +542,7 @@ const getReimbursementHistory = asyncHandler(async (req, res) => {
 // @desc    Upload a receipt
 // @route   POST /api/reimbursement/upload
 // @access  Private (Employee)
+/*
 const uploadReceipt = asyncHandler(async (req, res) => {
   if (!req.file) {
     return res.status(400).json({
@@ -560,6 +561,7 @@ const uploadReceipt = asyncHandler(async (req, res) => {
     },
   });
 });
+*/
 
 // Export all functions
 module.exports = {
@@ -573,5 +575,5 @@ module.exports = {
   cancelReimbursement,
   getReimbursementSummary,
   getReimbursementHistory,
-  uploadReceipt
+  // uploadReceipt
 }; 
