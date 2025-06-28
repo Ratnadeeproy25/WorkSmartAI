30ab6ee29e88e6de70184ca1c97d2aad4cabc3ad
Ratnadeep Roy <61830831+Ratnadeeproy25@users.noreply.github.com>
2025-06-28 16:20:29 +0530

    updated for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -96,7 +96,8 @@ app.use(
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:3001',
-        'http://127.0.0.1:3001'
+        'http://127.0.0.1:3001',
+        'https://worksmart-ai.vercel.app/'
       ];
       
       // In production, use environment variable
